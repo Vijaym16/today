458d233dc6c94dce5c31641177067a307c25e6b9
VIJAY M <78839120+Vijaym16@users.noreply.github.com>
2022-08-26 22:47:48 +0530

    Add files via upload

---
+++ today.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class today {
+    public static void main(String[] args) {
+        
+        Date today = new Date();
+        System.out.println(today);
+    }
+}
